35118bc8a77ecf7e54820c5645be82bb6bd5f21b
Emerson Barros <emerson.ebsilva18@gmail.com>
2020-09-03 01:52:52 -0300

    Implementado Slim

--- index.php
+++ index.php
@@ -2,14 +2,25 @@
 
 require_once("config.php");
 
-$usuario = new Model();
+//$usuario = new Model();
 
 //$usuario->insere("050", "EMERSON", "M", "4050", "LATITUTE", "DELL");
 
-$usuario->consultaGeral();
+//$usuario->consultaGeral();
 
 //$usuario->updateUso("108", "4050", NULL, "1412");
 
 //$usuario->deleteUso("50", "4050");
+
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+$app->get('/consulta', function(){
+    $usuario = new Model();
+    $usuario->consultaGeral();
+});
+
+$app->run();
+
 
 ?>
